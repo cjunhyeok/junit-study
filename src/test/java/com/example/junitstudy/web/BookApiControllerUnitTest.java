4784716914eb9efde933f9ac08125ac52218a48d
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-05 21:38:05 +0900

    Add deleteTest in BookApiControllerUnitTest

--- src/test/java/com/example/junitstudy/web/BookApiControllerUnitTest.java
+++ src/test/java/com/example/junitstudy/web/BookApiControllerUnitTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @WebMvcTest(BookApiController.class)
 public class BookApiControllerUnitTest {
@@ -73,12 +74,29 @@ public class BookApiControllerUnitTest {
         // stub
         given(bookService.findOne(1L)).willReturn(bookResponseDto);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/book/1")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/book/{id}", 1L)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.body.title").value("title"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.body.author").value("author"));
+    }
+
+    @Test
+    void deleteTest() throws Exception {
+
+        // given
+        Long bookId = 1L;
+
+        // DELETE 요청 및 응답 검증
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/book/{id}", bookId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"));
+
+        // bookService의 delete 메서드가 bookId 인자로 호출되었는지 확인할 수도 있습니다.
+         verify(bookService).delete(bookId);
     }
 }
