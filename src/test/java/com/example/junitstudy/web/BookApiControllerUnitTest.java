75dba707ee9fe012c2444c17f2dc73b455a5fc46
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-05 21:50:01 +0900

    Add saveBook in BookApiControllerUnitTest

--- src/test/java/com/example/junitstudy/web/BookApiControllerUnitTest.java
+++ src/test/java/com/example/junitstudy/web/BookApiControllerUnitTest.java
@@ -6,19 +6,23 @@ import com.example.junitstudy.web.dto.response.BookListResponseDto;
 import com.example.junitstudy.web.dto.response.BookResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookApiController.class)
 public class BookApiControllerUnitTest {
@@ -53,13 +57,13 @@ public class BookApiControllerUnitTest {
         // GET 요청 응답 검증
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/book")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.items[0].title").value("title"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.items[1].title").value("title2"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.items[0].author").value("author"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.items[1].author").value("author2"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("success"))
+                .andExpect(jsonPath("$.body.items[0].title").value("title"))
+                .andExpect(jsonPath("$.body.items[1].title").value("title2"))
+                .andExpect(jsonPath("$.body.items[0].author").value("author"))
+                .andExpect(jsonPath("$.body.items[1].author").value("author2"));
     }
 
     @Test
@@ -77,11 +81,11 @@ public class BookApiControllerUnitTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/book/{id}", 1L)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.title").value("title"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.body.author").value("author"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("success"))
+                .andExpect(jsonPath("$.body.title").value("title"))
+                .andExpect(jsonPath("$.body.author").value("author"));
     }
 
     @Test
@@ -93,9 +97,9 @@ public class BookApiControllerUnitTest {
         // DELETE 요청 및 응답 검증
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/book/{id}", bookId)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("success"));
 
         // bookService의 delete 메서드가 bookId 인자로 호출되었는지 확인할 수도 있습니다.
          verify(bookService).delete(bookId);
@@ -114,11 +118,36 @@ public class BookApiControllerUnitTest {
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/book/{id}", bookId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(bookSaveRequestDto)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("success"));
 
         // bookService의 update 메서드가 bookId와 bookSaveRequestDto 인자로 호출되었는지 확인할 수도 있습니다.
         // verify(bookService).update(bookId, bookSaveRequestDto);
+    }
+
+    @Test
+    void saveBook() throws Exception {
+        // Arrange
+        BookSaveRequestDto dto = new BookSaveRequestDto();
+        dto.setTitle("Book Title");
+        dto.setAuthor("Book Author");
+
+        BookResponseDto responseDto = new BookResponseDto(/* fill in with relevant values */);
+        when(bookService.registerBook(ArgumentMatchers.any(BookSaveRequestDto.class)))
+                .thenReturn(responseDto);
+
+        // Act
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)));
+
+        // Assert
+        resultActions.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("success"))
+                .andExpect(jsonPath("$.body").exists())
+                .andExpect(jsonPath("$.body").isNotEmpty());
     }
 }
